9537b886d15b9bb946eb8b3c5aebd1b584916a7a
mthtitumir <titumirgccc@gmail.com>
2024-06-30 21:09:45 +0600

    project module set up done

--- src/app/modules/project/project.route.ts
+++ src/app/modules/project/project.route.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import { ProjectControllers } from './project.controller';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
+
+router.post(
+    '/',
+    auth(),
+    ProjectControllers.addProject,
+  );
 
 export const ProjectRoutes = router;
